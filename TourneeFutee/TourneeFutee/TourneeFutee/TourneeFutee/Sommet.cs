66db4b2b94acecccecb774f32fdad768bbf591fe
Raphaël MOMAS <raphael.momas@edu.devinci.fr>
2026-02-17 17:39:10 +0100

    Classe sommet complétés

--- TourneeFutee/TourneeFutee/TourneeFutee/TourneeFutee/Sommet.cs
+++ TourneeFutee/TourneeFutee/TourneeFutee/TourneeFutee/Sommet.cs
@@ -9,5 +9,33 @@ namespace TourneeFutee
     internal class Sommet
     {
 
+        private string nom;
+        private float valeur;
+        public Sommet(string nom, float valeur)
+        {
+            this.nom = nom;
+            this.valeur = valeur;
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+        }
+
+        public float Valeur
+        {
+            get 
+            { 
+                return valeur; 
+            }
+            set 
+            {
+                this.valeur = value; 
+            }
+
+        }
     }
 }
